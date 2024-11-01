5b68c543415ab2ea9fc8ecabb48ab09bdf3250fd
mark <lopkophacked@protonmail.com>
2024-11-01 00:11:30 +0000

    add config config file

---
+++ config.py
@@ -0,0 +1,22 @@
+import logging
+
+from pydantic_settings import BaseSettings 
+
+logging.basicConfig(
+    format="%(levelname)s: %(asctime)s: %(message)s",
+    filename="logs/logs.txt",
+    level=logging.INFO,
+)
+
+logging.getLogger("uvicorn.access").disabled = True
+
+logger = logging.getLogger("uvicorn")
+logger.setLevel(logging.getLevelName(logging.DEBUG))
+
+
+class Settings(BaseSettings):
+    DEBUG: bool
+    REPO_PATH: str
+
+
+settings = Settings(_env_file=".env")
