60ee1537be64bccabd5a9595acba1faa05cc0606
mark <lopkophacked@protonmail.com>
2024-11-03 23:34:03 +0000

    create custom exception

--- app/exceptions.py
+++ app/exceptions.py
@@ -4,3 +4,12 @@ class ErrorStatus(Enum):
     ok = 0
     error = 1
 
+
+class LogicalError(Exception):
+    def __init__(self, msg, *args, **kwargs):            
+        self.msg = f"LogicalError: {msg}"
+        super().__init__(msg, args, kwargs)
+
+    def __str__(self):
+        return self.msg
+
