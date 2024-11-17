ba0621c7e5423f1bcc94b7eb7bce52c56a90d5df
mark <lopkophacked@protonmail.com>
2024-11-17 14:52:28 +0000

    fix bugs and refactor main.py

--- app/main.py
+++ app/main.py
@@ -4,14 +4,15 @@ import os
 
 from blacksheep import FromJSON
 from blacksheep import Application, get, post, put, delete
-from blacksheep.server.responses import ok, created, file, json
+from blacksheep.server.responses import created, json
 
 sys.path.append(os.path.abspath('../'))
 
 from config import settings
-from app.utils import show_file, commit, checkout_branch, add_file, branch_exists, write_note, merge, create_branch, delete_branch, list_files, get_current_branch
+from app.utils import show_file, commit, checkout_branch, add_file, branch_exists, write_note, merge, create_branch, delete_branch, list_files, get_current_branch, file_exists
 from app.exceptions import LogicalError
 from app.middlewares import exception_handler_middleware
+from app.cps import mask_conflicts
 
 
 app = Application()
@@ -25,9 +26,10 @@ app.use_cors(
 )
 
 
-# create branch, make changes, delete branch
 @get('/apiv1/get-note')
 def get_note(note_path: str, branch_name: str):
+    file_exists(settings.REPO_PATH, note_path, branch_name)
+
     note = show_file(settings.REPO_PATH, note_path, branch_name)
 
     return json({"note": note})
@@ -57,19 +59,25 @@ def create_note(note_path: str, note_value: str):
     create_branch(settings.REPO_PATH, branch_name)
     write_note(settings.REPO_PATH, note_path, note_value)
     add_file(settings.REPO_PATH, note_path)
-    commit(settings.REPO_PATH, branch_name, note_path, "msg")
+    commit(settings.REPO_PATH, note_path, "msg")
 
     conflict = merge(settings.REPO_PATH, settings.MAIN_BRANCH)
-    if not conflict:
-        checkout_branch(settings.REPO_PATH, settings.MAIN_BRANCH)
-        conflict = merge(settings.REPO_PATH, settings.MAIN_BRANCH)
-        if conflict:
-            raise LogicalError(f"Unexpected conflicts while merging {branch_name} into master")
-        delete_branch(settings.REPO_PATH, branch_name)
-    else:
-        commit(settings.REPO_PATH, branch_name, note_path, "msg")
+    if conflict:
+        mask_conflicts(settings.REPO_PATH, note_path)
+        commit(settings.REPO_PATH, note_path, f"conflict with {note_path}")
+        merge(settings.REPO_PATH, settings.MAIN_BRANCH)
+    
+        note_value = show_file(settings.REPO_PATH, note_path, settings.MAIN_BRANCH)
+        return json({"status": "conflict", "note": note_value})
+
+    checkout_branch(settings.REPO_PATH, settings.MAIN_BRANCH)
+    conflict = merge(settings.REPO_PATH, settings.MAIN_BRANCH)
+    if conflict:
+        raise LogicalError(f"Unexpected conflicts while merging {branch_name} into master")
+    delete_branch(settings.REPO_PATH, branch_name)
 
     return created()
+
 
 @dataclass
 class UpdateNoteInput:
@@ -92,19 +100,20 @@ def update_note(input_: FromJSON[UpdateNoteInput]):
     commit(settings.REPO_PATH, note_path, f"update {note_path}")
 
     conflict = merge(settings.REPO_PATH, settings.MAIN_BRANCH)
-    if not conflict:
-        checkout_branch(settings.REPO_PATH, settings.MAIN_BRANCH)
-        conflict_on_main = merge(settings.REPO_PATH, branch_name)
-        if conflict_on_main:
-            raise LogicalError(f"Unexpected conflicts while merging {branch_name} into {settings.MAIN_BRANCH}")
-        delete_branch(settings.REPO_PATH, branch_name)
-    else:
-        #todo: commit(settings.REPO_PATH, branch_name, note_path, "conflict")
-        with open(os.path.join(settings.REPO_PATH, note_path), "r") as f:
-            return json({"status": "conflict", "note": f.read()})
+    if conflict:
+        mask_conflicts(settings.REPO_PATH, note_path)
+        commit(settings.REPO_PATH, note_path, f"conflict with {note_path}")
+        merge(settings.REPO_PATH, settings.MAIN_BRANCH)
+    
+        note_value = show_file(settings.REPO_PATH, note_path, settings.MAIN_BRANCH)
+        return json({"status": "conflict", "note": note_value})
 
-
-    print('in the end')
+    checkout_branch(settings.REPO_PATH, settings.MAIN_BRANCH)
+    conflict_on_main = merge(settings.REPO_PATH, branch_name)
+    if conflict_on_main:
+        raise LogicalError(f"Unexpected conflicts while merging {branch_name} into {settings.MAIN_BRANCH}")
+    delete_branch(settings.REPO_PATH, branch_name)
+        
     return json({"status": "ok", "note": show_file(settings.REPO_PATH, note_path, settings.MAIN_BRANCH)})
 
 
